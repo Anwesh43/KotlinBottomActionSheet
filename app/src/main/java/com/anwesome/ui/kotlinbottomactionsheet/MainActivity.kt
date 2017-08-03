02d0363744184a555387692764041851b91ef2f1
Anwesh43 <anweshthecool0@gmail.com>
2017-08-03 18:32:34 +0530

    using ActionSheet in Activity

--- app/src/main/java/com/anwesome/ui/kotlinbottomactionsheet/MainActivity.kt
+++ app/src/main/java/com/anwesome/ui/kotlinbottomactionsheet/MainActivity.kt
@@ -2,10 +2,13 @@ package com.anwesome.ui.kotlinbottomactionsheet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesome.ui.kotlinactionsheet.ActionSheet
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ActionSheet.create(this)
+        ActionSheet.addToParent(this)
     }
 }
