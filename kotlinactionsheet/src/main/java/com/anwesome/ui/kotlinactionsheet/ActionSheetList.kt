5cce6e5b505a15824c70b316f62fab524634bfa9
Anwesh43 <anweshthecool0@gmail.com>
2017-08-03 18:51:16 +0530

    creating actionsheetmenu

--- kotlinactionsheet/src/main/java/com/anwesome/ui/kotlinactionsheet/ActionSheetList.kt
+++ kotlinactionsheet/src/main/java/com/anwesome/ui/kotlinactionsheet/ActionSheetList.kt
@@ -1,6 +1,13 @@
 package com.anwesome.ui.kotlinactionsheet
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 
@@ -27,5 +34,79 @@ class ActionSheetList(ctx:Context):ViewGroup(ctx) {
             w = Math.max(w,child.measuredWidth)
         }
         setMeasuredDimension(w,totalH)
+    }
+    fun addMenu(text:String,w:Int,h:Int) {
+        var menu = ActionSheetMenu(context,text)
+        addView(menu,LayoutParams(w,h))
+    }
+    class ActionSheetMenu(ctx:Context,var text:String,var scale:Float = 0.0f):View(ctx) {
+        var animator = MenuAnimator(this)
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        override fun onDraw(canvas:Canvas) {
+            var w = canvas.width.toFloat()
+            var h = canvas.height.toFloat()
+            paint.color = Color.GRAY
+            canvas.drawRect(0.0f,0.0f,w,h,paint)
+            paint.color = Color.WHITE
+            paint.textSize = w/20
+            canvas.drawText(text,w/2-paint.measureText(text)/2,h/2,paint)
+            canvas.save()
+            canvas.scale(scale,scale)
+            canvas.translate(w/2,h/2)
+            paint.color = Color.argb(150,0,0,0)
+            canvas.drawRect(-w/2,-h/2,w/2,h/2,paint)
+            canvas.restore()
+
+        }
+        fun update(factor:Float) {
+            scale = factor
+            postInvalidate()
+        }
+
+        override fun onTouchEvent(event: MotionEvent): Boolean {
+            when(event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    animator.start()
+                }
+            }
+            return true
+        }
+        class MenuAnimator(var v:ActionSheetMenu):AnimatorListenerAdapter(),ValueAnimator.AnimatorUpdateListener {
+            var endAnim = ValueAnimator.ofFloat(0.0f,1.0f)
+            var startAnim = ValueAnimator.ofFloat(1.0f,0.0f)
+            var dir = 0
+            var animated = false
+            init {
+                endAnim.addUpdateListener(this)
+                startAnim.addUpdateListener(this)
+                endAnim.addListener(this)
+                startAnim.addListener(this)
+                endAnim.duration = 500
+                startAnim.duration = 500
+            }
+            override fun onAnimationUpdate(vf:ValueAnimator) {
+                v.update(vf.animatedValue as Float)
+            }
+            override fun onAnimationEnd(animator:Animator) {
+                if(animated) {
+                    when(dir) {
+                        0 -> {
+                            endAnim.start()
+                            dir = 1
+                        }
+                        1 -> {
+                            dir = 0
+                            animated = false
+                        }
+                    }
+                }
+            }
+            fun start() {
+                if(!animated) {
+                    animated = true
+                    startAnim.start()
+                }
+            }
+        }
     }
 }
