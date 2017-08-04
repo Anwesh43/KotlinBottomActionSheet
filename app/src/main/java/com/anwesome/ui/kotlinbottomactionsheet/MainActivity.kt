fb7de4c5223f579c1a167a1c1e64b07c4193c382
Anwesh43 <anweshthecool0@gmail.com>
2017-08-04 16:55:24 +0530

    Using OnItemClickListener

--- app/src/main/java/com/anwesome/ui/kotlinbottomactionsheet/MainActivity.kt
+++ app/src/main/java/com/anwesome/ui/kotlinbottomactionsheet/MainActivity.kt
@@ -2,7 +2,9 @@ package com.anwesome.ui.kotlinbottomactionsheet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.anwesome.ui.kotlinactionsheet.ActionSheet
+import com.anwesome.ui.kotlinactionsheet.OnActionItemClickListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +13,13 @@ class MainActivity : AppCompatActivity() {
         ActionSheet.create(this)
         var txtMsgs = arrayOf("Add","Delete","Sort","Search")
         txtMsgs.forEach {
-            ActionSheet.addMenu(it)
+            ActionSheet.addMenu(it,ActionItemClickListener(this,it))
         }
         ActionSheet.addToParent(this)
+    }
+    class ActionItemClickListener(var activity: MainActivity,var text:String):OnActionItemClickListener {
+        override fun onClick() {
+            Toast.makeText(activity,text,Toast.LENGTH_SHORT).show()
+        }
     }
 }
